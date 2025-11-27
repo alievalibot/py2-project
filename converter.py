4e51dfa746460751343962c727658b713eae89c2
alievalibot <ali04042008@gmail.com>
2025-11-27 13:17:14 +0100

    Remove unused conversion coefficient lists for cleaner code

--- converter.py
+++ converter.py
@@ -4,17 +4,6 @@ from functions import convert_length, convert_weight, convert_temp, convert_cm_m
 
 sleep_time = 5
 
-length_objects = ["km", "miles"]
-weight_objects = ["kg", "pounds"]
-valute_objects = ["USD", "EUR"]
-cm_m_objects = ["cm", "m"]
-temp_units = ["C", "F"]
-
-
-length_coeffs = [0.621371, 1 / 0.621371]
-cm_m_coeffs = [0.01, 100]
-weight_coeffs = [2.20462, 1 / 2.20462]
-temp_coeffs = [[9/5, 32], [5/9, -32 * 5/9]]
 
 choice = None
 while choice != "0":
